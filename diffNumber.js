bbde1848b328efe629bacec1450710ef59175db4
andy <altenfreelance@gmail.com>
2019-11-11 13:59:06 -0500

    different number prompt

---
+++ diffNumber.js
@@ -0,0 +1,23 @@
+function getDifferentNumber (arr) {
+    // Get the smallest non negative integer NOT in the array
+    // Max array value is 2^32 -1
+    // Efficiency
+    // Prompt 1) You cannot modify the array
+    // Prompt 2) You can
+    
+    let numHash = {}
+
+    for(let i = 0; i< arr.length; i++){
+        numHash[arr[i]] = true;
+    }
+
+    let curNum = 0;
+    const MAX_INT = Math.pow(2,31) - 1
+    while(curNum <= MAX_INT){
+        if(!numHash[curNum]) return curNum;
+        curNum++;
+    }
+    throw console.error("Reached maximum int value :" + MAX_INT);
+}
+let arr = [0,1,2,3,4,5,-67,67, 45];
+console.log(getDifferentNumber(arr));
